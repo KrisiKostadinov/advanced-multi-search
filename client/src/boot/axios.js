020161075cc533955dff3ba5b5272c15ddb16573
krisikostadinov <krisi.199898@gmail.com>
2023-02-22 12:23:09 +0200

    Updated axios.js

--- client/src/boot/axios.js
+++ client/src/boot/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // localhost -> http://localhost:3000
-const api = axios.create({ baseURL: "http://localhost:3000" });
+const api = axios.create({ baseURL: "https://advanced-multi-search.onrender.com" });
 
 export { api };
